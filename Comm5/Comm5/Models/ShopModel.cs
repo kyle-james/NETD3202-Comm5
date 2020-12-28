c1dc4301d3950678b28a2bc2fec103a689a5de71
Kyle James <kyle.james1@dcmail.ca>
2020-12-28 15:59:46 -0500

    init commit

---
+++ Comm5/Comm5/Models/ShopModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Comm5.Models;
+
+namespace Comm5.Models
+{
+    public class ShopModel
+    {
+        [Key]
+        public int skuId { get; set; }
+        public string name { get; set; }
+        public double price { get; set; }
+
+        public ShopModel()
+
+        {
+
+        }
+        public ShopModel(int skuId, string name, double price)
+        {
+            this.skuId = skuId;
+            this.name = name;
+            this.price = price;
+        }
+            
+    }
+}
